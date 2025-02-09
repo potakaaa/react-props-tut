7b5408c086f5ff6e6226b5992180cbf9e7055c9f
potakaaa <helbirog@gmail.com>
2025-02-09 14:55:43 +0800

    set up shadcn and tailwind

---
+++ src/global/posts.ts
@@ -0,0 +1,19 @@
+export const dogPosts = [
+    {
+      title: "The Best Dog Breeds for Families",
+      description:
+        "Discover the top dog breeds that are great with kids and families.",
+      imageUrl: "/dog/dog1.webp",
+    },
+    {
+      title: "How to Train Your Dog",
+      description: "Effective training tips for teaching your dog new tricks.",
+      imageUrl: "/dog/dog2.webp",
+    },
+    {
+      title: "Dog Grooming 101",
+      description:
+        "Essential grooming techniques to keep your dog clean and healthy.",
+      imageUrl: "/dog/dog3.webp",
+    },
+  ];
